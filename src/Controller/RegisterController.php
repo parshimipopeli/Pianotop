4f6a22344c2a72ab9098e8e4e00799d30ca9056a
parshimipopeli <plamorski@simplonprod.co>
2020-11-27 15:39:30 +0100

    Add form register and encoderPassword

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,19 +2,43 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterController extends AbstractController
 {
     /**
      * @Route("/inscription", name="app_register")
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder): Response
     {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+
+            $password = $encoder->encodePassword($user, $user->getPassword());
+
+            $user->setPassword($password);
+
+            $em->persist($user);
+            $em->flush();
+        }
+
+
+
         return $this->render('register/index.html.twig', [
-            'controller_name' => 'RegisterController',
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,15 +6,51 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password')
-        ;
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre Prénom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre prenom'
+
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+                'attr' => [
+                    'placeholder' => 'merci de saisir votre email'
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent être identiques!',
+                'required' => true,
+                'first_options' => [
+                    'label' => "Votre mot de passe" ],
+                    'second_options' => [
+                        'label' => 'Confirmez votre mot de passe']
+                    
+                
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'label' => 'S \'inscrire'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
